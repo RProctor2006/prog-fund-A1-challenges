fa3de0273906f0c9d4807a7b45c6cbc41469e814
Riley Proctor <riley.proctor2006@gmail.com>
2024-10-03 12:17:18 +0100

    Assessment 1: Challenge 1
    The final result of the first challenge

---
+++ ProgFundChallenge1.cpp
@@ -0,0 +1,23 @@
+// ProgFundChallenge1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Assessment One, Challenge One
+    cout << "\t\t  |\n";
+    cout << "\t\t /|\\\n";
+    cout << "\t\t/ | \\     a boat\n";
+    cout << "\t       /  |  \\\n";
+    cout << "\t      / --|-- \\\n";
+    cout << "\t     /    |    \\\n";
+    cout << "\t    /-----|-----\\\n";
+    cout << "     |----\\       |      /----|\n";
+    cout << "      \\- - -------------- - -/\n";
+    cout << "       \\ - -- - - - - - -- -/\n";
+    cout << " /\\  /\\ \\/\\  /\\  /\\  /\\  /\\/ /\\  /\\\n";
+    cout << "/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\/  \\";
+}
